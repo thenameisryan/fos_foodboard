8449863c52fd7bd3f19c8593b56bf6ff4030bcdc
Ryan <31982621+thenameisryan@users.noreply.github.com>
2020-10-04 01:10:23 +0800

    favicon

--- client/main.php
+++ client/main.php
@@ -10,7 +10,10 @@
   <title>FoodBoard</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
-
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="img/favicon/site.webmanifest">
   <!-- Bootstrap core CSS -->
   <link href="assets/dist/css/bootstrap.css" rel="stylesheet">
 
