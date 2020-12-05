c257f36bab9751223382622238e1d923aa697a49
Ryan <31982621+thenameisryan@users.noreply.github.com>
2020-12-06 05:48:06 +0800

    update sql file

--- fos_foodboard.sql
+++ fos_foodboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2020 at 09:49 AM
+-- Generation Time: Dec 05, 2020 at 10:46 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -25,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `fos_admin`
+--
+
+CREATE TABLE `fos_admin` (
+  `uid` int(5) NOT NULL,
+  `admin_username` varchar(50) NOT NULL,
+  `admin_pass` varchar(50) NOT NULL,
+  `date_created` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fos_admin`
+--
+
+INSERT INTO `fos_admin` (`uid`, `admin_username`, `admin_pass`, `date_created`) VALUES
+(1, 'admin', 'MTIz', '04-10-2020 03:13:07');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `fos_cat`
 --
 
@@ -41,15 +61,14 @@ CREATE TABLE `fos_cat` (
 --
 
 INSERT INTO `fos_cat` (`uid`, `client_uid`, `cat_title`, `date_created`, `date_edited`) VALUES
-(1, 15, 'All', '12-10-2020 23:40:38', ''),
-(4, 15, 'Main', '31-10-2020 04:27:39', '31-10-2020 04:27:41'),
 (5, 17, 'All', '03-11-2020 21:51:56', ''),
 (6, 18, 'All', '05-11-2020 04:27:23', ''),
 (7, 19, 'All', '05-11-2020 04:46:58', ''),
 (8, 20, 'All', '05-11-2020 04:49:18', ''),
 (9, 21, 'All', '05-11-2020 04:56:55', ''),
 (10, 22, 'All', '06-11-2020 17:24:48', ''),
-(11, 23, 'All', '07-11-2020 22:36:47', '');
+(11, 23, 'All', '07-11-2020 22:36:47', ''),
+(12, 24, 'All', '06-12-2020 00:44:38', '');
 
 -- --------------------------------------------------------
 
@@ -75,10 +94,7 @@ CREATE TABLE `fos_client` (
 --
 
 INSERT INTO `fos_client` (`uid`, `client_username`, `client_email`, `client_pass`, `client_res_name`, `client_status`, `client_qr`, `client_qr_image`, `date_created`, `created_by`) VALUES
-(1, 'ryan', 'ryan@mail.com', 'MTIz', 'Spades', '', '', '', '04-10-2020 03:13:07', 'USER'),
-(2, 'ryan2', 'd@mail.com', 'MTIzNDU=', 'Spades2', '', '', '', '04-10-2020 04:10:10', 'USER'),
-(3, 'ryan17', 'mail@mail.com', 'b2tva29r', 'Lai Cafe', '', '', '', '04-10-2020 18:27:11', 'USER'),
-(4, 'test', 'test@mail.com', 'dGVzdA==', 'New Thing', '', '', '', '08-10-2020 13:35:11', 'USER'),
+(1, 'admin', 'ryan@mail.com', 'MTIz', 'Spades', '', '', '', '04-10-2020 03:13:07', 'USER'),
 (5, 'test123', 'test123@mail.com', 'dGVzdDEyMw==', 'McD', '', '', '', '08-10-2020 13:36:46', 'USER'),
 (6, 'bbg', 'bbg@mail.com', 'YmJn', 'Burger Bagus', '', '', '', '08-10-2020 13:38:21', 'USER'),
 (7, 'pelita', 'pelita@mail.com', 'MTIz', 'Pelita', '', '', '', '08-10-2020 13:41:07', 'USER'),
@@ -89,14 +105,14 @@ INSERT INTO `fos_client` (`uid`, `client_username`, `client_email`, `client_pass
 (12, 'kapitan', 'kapitan@mail.com', 'MTIz', 'Kapitan', '', '', '', '08-10-2020 13:52:50', 'USER'),
 (13, 'testtest', 'test24@mail.com', 'MTIz', 'testrun', '', '', '', '08-10-2020 14:03:01', 'USER'),
 (14, 'ryanlai', 'ryanlai@mail.com', 'MTIz', 'Burger Place', '', 'true', 'assets/qrcodes/Burger Place5f9b1005a6c6e.png', '08-10-2020 15:07:21', 'USER'),
-(15, 'testlocal', 'test232@mail.com', 'MTIz', 'local', '', 'true', 'assets/qrcodes/local5f9b0d31bbb22.png', '12-10-2020 23:40:38', 'USER'),
 (17, 'likethis', 'mypride@mail.com', 'MzIx', 'notlikethis', '', 'true', 'assets/qrcodes/notlikethis5fa1608d6772d.png', '03-11-2020 21:51:56', 'USER'),
 (18, 'name', 'mailer@mail.com', 'MTIz', 'nameless', '', 'false', '', '05-11-2020 04:27:23', 'USER'),
 (19, 'welcome', 'mail3@mail.com', 'MTIz', 'welcome', '', 'false', '', '05-11-2020 04:46:58', 'USER'),
 (20, 'dw', '123@mail.com', 'aGVsbG8=', 'dw', '', 'false', '', '05-11-2020 04:49:18', 'USER'),
 (21, 'helo23', 'helo35@mail.com', 'MTIz', 'helo23', '', 'false', '', '05-11-2020 04:56:55', 'USER'),
 (22, 'testing123', 'xxryanlaixx@gmail.com', 'MTIz', 'shit', '', 'false', '', '06-11-2020 17:24:48', 'USER'),
-(23, 'localtest', 'localtest@mail.com', 'MTIz', 'localtest', '', 'false', '', '07-11-2020 22:36:47', 'USER');
+(23, 'localtest', 'localtest@mail.com', 'MTIz', 'localtest', '', 'true', 'assets/qrcodes/localtest5fadaf15d901b.png', '07-11-2020 22:36:47', 'USER'),
+(24, 'admin_test1', 'admin_test1@mail.com', 'MTIzNA==', 'admin_test1', '', 'false', '', '06-12-2020 00:44:38', 'USER');
 
 -- --------------------------------------------------------
 
@@ -120,19 +136,17 @@ CREATE TABLE `fos_landing` (
 --
 
 INSERT INTO `fos_landing` (`uid`, `client_uid`, `landing_desc`, `landing_location`, `landing_contact`, `landing_image`, `date_created`, `date_edited`) VALUES
-(1, 0, 'Your restaurants description goes in here. This is a default description. You are able to change thi', 'Your restaurants location goes in here. This is a default location. You are able to change this in t', 'Your restaurants contact goes in here. This is a d', '', '08-10-2020 13:46:06', ''),
-(2, 0, 'Your restaurants description goes in here. This is a default description. You are able to change thi', 'Your restaurants location goes in here. This is a default location. You are able to change this in t', 'Your restaurants contact goes in here. This is a d', '', '08-10-2020 13:48:19', ''),
 (3, 12, 'Your restaurants description goes in here. This is a default description. You are able to change thi', 'Your restaurants location goes in here. This is a default location. You are able to change this in t', 'Your restaurants contact goes in here. This is a d', '', '08-10-2020 13:52:50', ''),
 (4, 13, 'dwdaw', 'dwadwa', '(213) 213-21__', 'assets/landing-bg/card-img-3.jpg', '08-10-2020 14:03:01', '08-10-2020 14:34:25'),
 (5, 14, 'This is a default description. You are able to change this in the dashboard.', 'This is a default location. You are able to change this in the dashboard.', '000-0000000', '', '08-10-2020 15:07:21', ''),
-(6, 15, 'good home cook food', 'home', '(016) 415-7901', 'assets/landing-bg/smiling sun.jpg', '12-10-2020 23:40:38', '31-10-2020 04:30:43'),
 (7, 17, 'This is a default description. You are able to change this in the dashboard.', 'This is a default location. You are able to change this in the dashboard.', '000-0000000', 'assets/landing-bg/card-img-3.jpg', '03-11-2020 21:51:56', ''),
 (8, 18, 'nameless desc', 'home', '(222) 222-2222', 'assets/landing-bg/card-img-3.jpg', '05-11-2020 04:27:23', ''),
 (9, 19, 'welcome', 'welcome', '(000) 000-0000', 'assets/landing-bg/card-img-3.jpg', '05-11-2020 04:46:58', ''),
 (10, 20, 'dw', 'dw', '(222) 222-2222', 'assets/landing-bg/sadge.jpg', '05-11-2020 04:49:18', ''),
 (11, 21, '', '', '', 'assets/landing-bg/smiling sun.jpg', '05-11-2020 04:56:55', ''),
 (12, 22, 'test', '', '', 'assets/landing-bg/sadge.jpg', '06-11-2020 17:24:48', ''),
-(13, 23, 'The patty is pan fried, grilled, smoked or flame broiled. Hamburgers are often served with cheese,\r\n                lettuce, tomato, onion, pickles, b', '1-1-22, Ideal Avenue, Medan Kampung Relau 1, Kampung Seberang Paya, 11900 Bayan Lepas, Pulau Pinang', '(012) 478-9987', 'assets/landing-bg/promo-bg.jpg', '07-11-2020 22:36:47', '07-11-2020 22:53:01');
+(13, 23, 'The patty is pan fried, grilled, smoked or flame broiled. Hamburgers are often served with cheese,\r\n                lettuce, tomato, onion, pickles, b', '1-1-22, Ideal Avenue, Medan Kampung Relau 1, Kampung Seberang Paya, 11900 Bayan Lepas, Pulau Pinang', '(012) 478-9987', 'assets/landing-bg/promo-bg.jpg', '07-11-2020 22:36:47', '07-11-2020 22:53:01'),
+(14, 24, 'admin_test1', 'penang,malaysia1', '222111', 'client/assets/landing-bg/sadge.jpg', '06-12-2020 00:44:38', '06-12-2020 02:36:06');
 
 -- --------------------------------------------------------
 
@@ -148,6 +162,30 @@ CREATE TABLE `fos_order` (
   `order_status` varchar(50) NOT NULL,
   `date_created` varchar(50) NOT NULL,
   `date_edited` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fos_order`
+--
+
+INSERT INTO `fos_order` (`uid`, `client_uid`, `order_number`, `order_cusid`, `order_status`, `date_created`, `date_edited`) VALUES
+(1, 23, 1, 1, 'COMPLETE', '13-11-2020 01:44:47', ''),
+(2, 23, 2, 2, 'PENDING', '13-11-2020 10:00:47', ''),
+(3, 23, 3, 3, 'PENDING', '13-11-2020 10:20:47', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fos_orderitem`
+--
+
+CREATE TABLE `fos_orderitem` (
+  `uid` int(5) NOT NULL,
+  `order_number` int(5) NOT NULL,
+  `prod_uid` int(5) NOT NULL,
+  `prod_name` varchar(50) NOT NULL,
+  `prod_quantity` varchar(50) NOT NULL,
+  `prod_price` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -188,17 +226,17 @@ INSERT INTO `fos_prod` (`uid`, `client_uid`, `prod_name`, `prod_image`, `prod_de
 (15, 1, 'big image', 'assets/uploads/', '', '5', '08-10-2020 11:28:15', NULL),
 (16, 1, 'big image ', 'assets/uploads/GAMEFOUND.png', '', '4', '08-10-2020 11:29:15', NULL),
 (17, 1, 'big image', 'assets/uploads/dog.jpg', '', '2', '08-10-2020 11:29:39', NULL),
-(18, 3, 'capsican', 'assets/uploads/lp-2.jpg', 'spices', '6', '08-10-2020 11:35:41', NULL),
 (19, 1, 'decimal', 'assets/uploads/alert.png', '', '4.36', '08-10-2020 12:01:35', NULL),
-(20, 3, 'noodle', 'assets/uploads/', '', '2', '08-10-2020 12:08:08', NULL),
 (21, 1, 'burger', 'assets/uploads/', '', 'RM16__', '08-10-2020 12:49:16', NULL),
 (22, 1, 'burger special', 'assets/uploads/', '', '2.00', '08-10-2020 12:54:29', NULL),
 (23, 1, 'cheese fries', 'assets/uploads/', 'yum', '5', '08-10-2020 13:07:54', NULL),
 (24, 12, 'tikka massala', 'assets/uploads/', '', '4', '08-10-2020 13:57:16', NULL),
 (25, 14, 'special burger', 'assets/uploads/lp-3.jpg', 'yummy', '11', '08-10-2020 15:11:19', NULL),
-(33, 15, 'noodle', 'assets/uploads/smiling sun.jpg', 'chicken soup', '10', '02-11-2020 01:23:12', NULL),
-(34, 23, 'Cheeseburger', 'assets/images/eco-product-img-1.png', 'Mozzarella cheese with french fries on the side', '14', '07-11-2020 23:06:17', NULL),
-(35, 23, 'Chicken Noodle', 'assets/images/eco-product-img-1.png', 'Chicken Soup ', '12', '08-11-2020 17:23:39', NULL);
+(34, 23, 'Cheeseburger', 'assets/uploads/cheeseburger.png', 'Mozzarella cheese with french fries on the side', '14', '07-11-2020 23:06:17', NULL),
+(35, 23, 'Chicken Noodle', 'assets/uploads/chickennoodle.jpg', 'Chicken Soup ', '12', '08-11-2020 17:23:39', NULL),
+(36, 23, 'Spaghetti Carbonara', 'assets/uploads/carbonara.jpg', 'Smoked beef spaghetti carbonara is classic pasta dish for people that can\'t eat pork product.', '22', '13-11-2020 04:49:38', NULL),
+(37, 23, 'Chicken Chop', 'assets/uploads/cc.jpg', 'Served with brown sauce', '10', '13-11-2020 04:50:40', NULL),
+(38, 23, 'Char Koay Teow', 'assets/uploads/ckt.jpg', 'Chinese lap cheong (sausage), eggs, bean sprouts, and chives in a mix of soy sauce.', '9', '13-11-2020 04:52:17', NULL);
 
 -- --------------------------------------------------------
 
@@ -216,6 +254,15 @@ CREATE TABLE `fos_queue` (
   `queue_status` varchar(50) NOT NULL,
   `date_created` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fos_queue`
+--
+
+INSERT INTO `fos_queue` (`uid`, `client_uid`, `queue_number`, `queue_cus_name`, `queue_cus_contact`, `queue_cus_size`, `queue_status`, `date_created`) VALUES
+(149, 23, '1', 'dwd', '2', '2', 'READY', '03-12-2020 02:11:51'),
+(151, 23, '2', 'rdwd', '2', '2', 'WAITING', '03-12-2020 13:08:44'),
+(152, 23, '3', 'ddd', '213', '2', 'WAITING', '03-12-2020 13:10:23');
 
 -- --------------------------------------------------------
 
@@ -245,7 +292,8 @@ INSERT INTO `fos_rcvrpass` (`uid`, `email`, `token`) VALUES
 (12, 'xxryanlaixx@gmail.com', '732bb2071df11a4b85032345259c53a00456d0e5e9282a1d26'),
 (13, 'xxryanlaixx@gmail.com', 'f036664cd836e259160aea25cac931ea9dc845229fbf4af1bac89d5031b6b15169457ddc135807a8cc471b6334faa359bdd7'),
 (14, 'xxryanlaixx@gmail.com', '33481e8253004a7ced63ccbcce951ea244aed5d138fb40ba38fc7b2b2e9072b23aebd7e422be1bdf20351e6182b4c10dab14'),
-(15, 'xxryanlaixx@gmail.com', '81a3acd2ef6b5709ce89f0f466cb555a30bac3a7c43238ed8a7bd1a5f41c191d8f7af4b6c2acd65a058a774cad7a541ee4d1');
+(15, 'xxryanlaixx@gmail.com', '81a3acd2ef6b5709ce89f0f466cb555a30bac3a7c43238ed8a7bd1a5f41c191d8f7af4b6c2acd65a058a774cad7a541ee4d1'),
+(16, 'xxryanlaixx@gmail.com', '4c9e8a4330e5c0405642916ade76aece0eadd15b5624589111657fc90f5fcf72714598d3b102476d62a8362086fae92bc45b');
 
 -- --------------------------------------------------------
 
@@ -271,13 +319,18 @@ INSERT INTO `fos_review` (`uid`, `client_uid`, `review_content`, `review_stars`,
 (3, 19, 'dfwa', '5', '07-11-2020 17:27:24'),
 (4, 19, 'y', '4', '07-11-2020 17:59:42'),
 (5, 12, 'good\r\n', '1', '07-11-2020 18:17:57'),
-(6, 23, 'lousy service', '1', '10-11-2020 05:48:20'),
-(7, 23, 'helo', '2', '10-11-2020 16:22:27'),
+(7, 23, 'will come again and good service', '4', '10-11-2020 16:22:27'),
 (8, 23, 'good food nice environment', '5', '11-11-2020 17:20:08');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `fos_admin`
+--
+ALTER TABLE `fos_admin`
+  ADD PRIMARY KEY (`uid`);
 
 --
 -- Indexes for table `fos_cat`
@@ -301,6 +354,12 @@ ALTER TABLE `fos_landing`
 -- Indexes for table `fos_order`
 --
 ALTER TABLE `fos_order`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `fos_orderitem`
+--
+ALTER TABLE `fos_orderitem`
   ADD PRIMARY KEY (`uid`);
 
 --
@@ -333,46 +392,58 @@ ALTER TABLE `fos_review`
 --
 
 --
+-- AUTO_INCREMENT for table `fos_admin`
+--
+ALTER TABLE `fos_admin`
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `fos_cat`
 --
 ALTER TABLE `fos_cat`
-  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `fos_client`
 --
 ALTER TABLE `fos_client`
-  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `fos_landing`
 --
 ALTER TABLE `fos_landing`
-  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `fos_order`
 --
 ALTER TABLE `fos_order`
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `fos_orderitem`
+--
+ALTER TABLE `fos_orderitem`
   MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `fos_prod`
 --
 ALTER TABLE `fos_prod`
-  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `fos_queue`
 --
 ALTER TABLE `fos_queue`
-  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `uid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=154;
 
 --
 -- AUTO_INCREMENT for table `fos_rcvrpass`
 --
 ALTER TABLE `fos_rcvrpass`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `fos_review`
